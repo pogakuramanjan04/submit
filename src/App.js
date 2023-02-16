4c742e561bbf6448f43fd0715d7462cb5d38ff6d
pogakuramanjan04 <125488490+pogakuramanjan04@users.noreply.github.com>
2023-02-16 11:45:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [d, u] = useState(true);
+  const [a, ua] = useState("plz type");
+
+  function sub(e) {
+    let v = e.target.value;
+    console.log(v);
+    // console.log(v)
+    if (v === "") {
+      u(true);
+      ua("ok");
+    } else {
+      u(false);
+      u("not entered");
+    }
+  }
+
+  function r(e) {
+    let v = e.target.value;
+    //  console.log(v)
+    if (v === "") {
+      u(true);
+    } else {
+      u(false);
+    }
+  }
+  return (
+    <div className="App">
+      <input type="text" onChange={r} />
+      <button type="button" onclick={sub} disabled={d}>
+        submit
+      </button>
+      {a}
+    </div>
+  );
+}
